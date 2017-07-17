2ed4d9da64242b6c569d9f1cd5e4d83c7630513c
Aman Adhikari <adhikariaman01@gmail.com>
2017-07-16 21:45:20 -0500

    Project Init

---
+++ src/main/java/com/example/hecate/HecateApplication.java
@@ -0,0 +1,12 @@
+package com.example.hecate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HecateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HecateApplication.class, args);
+	}
+}
