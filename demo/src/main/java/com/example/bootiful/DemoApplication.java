6eb7b0859ab583e6c1af9ef79801ff42563808d6
Aman Adhikari <adhikariaman01@gmail.com>
2017-08-20 23:31:10 -0500

    Getting config from config-server

--- demo/src/main/java/com/example/bootiful/DemoApplication.java
+++ demo/src/main/java/com/example/bootiful/DemoApplication.java
@@ -1,14 +1,18 @@
 package com.example.bootiful;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -42,6 +46,18 @@ class DummyDataCLR implements CommandLineRunner {
 
 	}
 }*/
+
+@RefreshScope
+@RestController
+class MessageRestController {
+	@Value("${message}")
+	private String msg;
+
+	@RequestMapping("/message")
+	String message() {
+		return this.msg;
+	}
+}
 
 @RepositoryRestResource
 interface ReservationRepository extends JpaRepository<Reservation,Long> {
